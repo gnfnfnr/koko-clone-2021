03c4ab350546efaf57d1aa855957dd978c67f654
ramda <91455804+gnfnfnr@users.noreply.github.com>
2021-11-02 22:37:53 +0900

    login_fn

--- javascript/index_login.js
+++ javascript/index_login.js
@@ -3,15 +3,17 @@ const percent = document.querySelector(".status-bar > .percent > span");
 
 function setTime(){
   const today = new Date();
-  let hours = today.getHours();
+  let hours =today.getHours();
   const minutes = today.getMinutes();
-  const seconds = today.getSeconds();
+  const seconds = String(today.getSeconds()).padStart(2, "0");
   let MODE = "AM";
- if (hours > 13){
-   hours = hours - 12;
+ if (hours >= 12){
+   if (hours >= 13) {
+     hours = hours - 12;
+   }
    MODE = "PM"
  }
- time.innerText = `${MODE} ${hours}:${minutes}:${seconds}`;
+ time.innerText = `${MODE} ${hours}:${String(minutes).padStart(2, "0")}:${seconds}`;
  percent.innerText = `${minutes}%`
 }
 
